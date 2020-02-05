c39bccb86c6ebf2f97f81dbd119734bec7fb0129
mgh_2000 <m.aghasemzadeh@yahoo.com>
2020-02-05 05:27:51 +0330

    blanket and train and entity buttons activated.

---
+++ app/src/main/java/com/example/paboosyar/RecreationalActivity.java
@@ -0,0 +1,14 @@
+package com.example.paboosyar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecreationalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recreational);
+    }
+}
